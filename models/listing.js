b153bd77b0a2a3d41eb829c60378608737d05e02
Namrata99-developer <g3namrata@gmail.com>
2026-03-15 20:08:40 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -10,10 +10,21 @@ const listingSchema = new Schema({
         type: String,
         required: true
     },
+    // image: {
+    //     type: String,
+    //     default: "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D",
+    //     set: (v) => v === " " ? "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" : v
+    // },
     image: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D",
-        set: (v) => v === " " ? "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" : v
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" : v
+        }
     },
     price: Number,
     location: String,
