5967a190b19399d35ab00bc4e6b9f949e3a216c1
Namrata99-developer <g3namrata@gmail.com>
2026-08-23 18:12:37 +0530

    Rendering of reviews in show.ejs creating cards type div and populate the review in app.js show route

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
 
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
 }));
 
