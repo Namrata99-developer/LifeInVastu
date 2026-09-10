6344f0422537829448439a4951f217583d2b22e0
Namrata99-developer <g3namrata@gmail.com>
2026-09-10 21:56:44 +0530

    Authorization of reviews created by which author and only she can delete that particular review

--- routes/review.js
+++ routes/review.js
@@ -4,14 +4,15 @@ const router = express.Router({ "mergeParams": true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 
 
 //POST Review Routes
-router.post("/", validateReview, wrapAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let review = new Review(req.body.review);
+    review.author = req.user._id;
 
     listing.reviews.push(review);
 
@@ -24,7 +25,7 @@ router.post("/", validateReview, wrapAsync(async (req, res) => {
 }));
 
 //delete Review Route
-router.delete("/:reviewId", wrapAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
 
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
