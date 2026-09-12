50fb0bf56e8af0330a387e7fd56d4468e6e3914c
Namrata99-developer <g3namrata@gmail.com>
2026-09-12 15:29:57 +0530

    Authorization of delete review bug solved

--- middleware.js
+++ middleware.js
@@ -50,13 +50,24 @@ module.exports.validateReview = (req, res, next) => {
     }
 }
 
+// module.exports.isReviewAuthor = async (req, res, next) => {
+//     let { id, reviewId } = req.params;
+//     let review = await Review.findById(reviewId);
+//     if (!review.author.equals(res.locals.currentUser._id)) {
+//         req.flash("error", "You are not the author of this review!");
+//         return res.redirect(`/listings/${id}`);
+//     } else {
+//         next();
+//     }
+// }
 module.exports.isReviewAuthor = async (req, res, next) => {
     let { id, reviewId } = req.params;
     let review = await Review.findById(reviewId);
-    if (!review.author.equals(res.locals.currentUser._id)) {
+
+    if (!review.author.equals(req.user._id)) {
         req.flash("error", "You are not the author of this review!");
         return res.redirect(`/listings/${id}`);
-    } else {
-        next();
     }
-}
+
+    next();
+};
