91037f33d6160c17d7b99355b4ecd38774660da6
Namrata99-developer <g3namrata@gmail.com>
2026-09-13 22:06:45 +0530

    uploading image as a file jpg,jpeg or png , using cloudinary for free cloud storage which generate a link that save to mongodb

--- models/listing.js
+++ models/listing.js
@@ -17,15 +17,8 @@ const listingSchema = new Schema({
     //     set: (v) => v === " " ? "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" : v
     // },
     image: {
-        filename: {
-            type: String,
-            default: "listingimage",
-        },
-        url: {
-            type: String,
-            default: "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D",
-            set: (v) => v === "" ? "https://images.unsplash.com/photo-1764760764875-bae4e3a58835?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1M3x8fGVufDB8fHx8fA%3D%3D" : v
-        }
+        url: String,
+        filename: String,
     },
     price: Number,
     location: String,
